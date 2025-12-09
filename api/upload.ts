b1a2b6ae2179bb8dbd5b90d7f6f870a4ead37989
TanHoangarc <tanhoangarc@gmail.com>
2025-12-09 08:36:11 +0700

    Update upload URL from local IP to localhost

--- api/upload.ts
+++ api/upload.ts
@@ -4,7 +4,7 @@ export async function uploadFile(file: File) {
     const formData = new FormData();
     formData.append("file", file); // <-- phải đúng tên "file"
 
-    const res = await axios.post("http://192.168.1.110/upload", formData, {
+    const res = await axios.post("http://127.0.0.1:3001/upload", formData, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
@@ -12,4 +12,5 @@ export async function uploadFile(file: File) {
 
     return res.data;
 }
+
 
