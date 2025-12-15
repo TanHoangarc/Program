c8ede0e1b68f11fdf7a069abcdb842560e1187e1
TanHoangarc <tanhoangarc@gmail.com>
2025-12-15 15:51:38 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -418,7 +418,7 @@ const App: React.FC = () => {
       setCustomReceipts(newReceipts);
 
       await handleRejectRequest(requestId);
-      alert("Đã duyệt và cập nhật dữ liệu thành công!");
+
   };
 
   useEffect(() => {
