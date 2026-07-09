8fa222e229b4c75d89b60590fd0317155672345b
TanHoangarc <tanhoangarc@gmail.com>
2026-07-09 10:00:25 +0700

    Add files via upload

--- server.ts
+++ server.ts
@@ -101,7 +101,7 @@ async function startServer() {
             pending: [], staff: [], longHoangOrders: [],
             headerMessages: [], headerNotifications: [], headerUpdates: [],
             customReceipts: [], salaries: [], yearlyConfigs: {},
-            lockedIds: [], processedRequestIds: []
+            lockedIds: [], processedRequestIds: [], authorizations: []
         });
     }
 
@@ -141,7 +141,7 @@ async function startServer() {
                 pending: [], staff: [], longHoangOrders: [],
                 headerMessages: [], headerNotifications: [], headerUpdates: [],
                 customReceipts: [], salaries: [], yearlyConfigs: {},
-                lockedIds: [], processedRequestIds: []
+                lockedIds: [], processedRequestIds: [], authorizations: []
             };
             
             const result = await action(dbState);
@@ -417,6 +417,7 @@ async function startServer() {
                 if (safeData.processedRequestIds) dbState.processedRequestIds = safeData.processedRequestIds;
                 if (safeData.salaries) dbState.salaries = mergeLists(dbState.salaries || [], safeData.salaries);
                 if (safeData.yearlyConfigs) dbState.yearlyConfigs = safeData.yearlyConfigs; 
+                if (safeData.authorizations) dbState.authorizations = mergeLists(dbState.authorizations || [], safeData.authorizations);
                 if (safeData.longHoangOrders) dbState.longHoangOrders = mergeLists(dbState.longHoangOrders || [], safeData.longHoangOrders);
 
             } else if (isDocs) {
